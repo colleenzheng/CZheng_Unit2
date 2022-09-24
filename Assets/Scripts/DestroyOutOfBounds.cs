78a9a38b6b29853ec3b252ebc0558f1d038cbce0
Colleen Zheng <colleenzheng123@gmail.com>
2022-09-24 00:37:27 -0700

    Colleen's Unit 2 Lesson D

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -21,6 +21,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         } else if (transform.position.z < bottomOfScene)
         {
             Destroy(gameObject);
+            Debug.Log("Game Over!");
         }
     }
 }
